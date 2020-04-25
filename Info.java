9ab62bc97154ca2485ba36299f2be056ea3fdb93
Luna Liu <53630450+lunalqk@users.noreply.github.com>
2020-04-24 18:40:49 -0700

    Create Info.java

---
+++ Info.java
@@ -0,0 +1,66 @@
+public class Info {
+    public static String[] name = {
+            "Start",
+            "Debt",
+            "Unwell",
+            "Hospital",
+            "Cruise",
+            "UK",
+            "Canada",
+            "Iran",
+            "Spain",
+            "Italy",
+            "China",
+            "US",
+            "False Information",
+            "Prevention",
+            "Treatment",
+            "Globally"
+    };
+
+    public static String[] desc = {
+            "Welcome to COVID-19 Monopoly!",
+            "The US has lost $4 trillion, China has lost $393 billion, and globally there has been a $6 trillion lost.",
+            "Light symptoms: cough, fever, tiredness, sore throat, diarrhea, aches, and pains",
+            "Difficulty in breathing",
+            "Diamond Princess: More than 40 Canadians tested positive for COVID-19; 700 overall tested positive; 11 died. Grand Princess: 103 tested positive; 2 died. Coral Princess: 12 tested positive. Ruby Princess: 600 cases; 15 positive. There are 100 cruise ships still stuck in water",
+            "Around 129,000 cases; 17,000 deaths. PM Johnson is out of the Intensive Care Unit and is the first world leader to be hospitalized due to COVID-19. The economy is potentially the worst since WWII. Johnson has handed control to Foreign Minister Dominic Raab, who says it is too early to end the lockdown. Bank of England has agreed to finance the government in response to COVID-19. Government has pledged tens of billions of pounds that will be used to help businesses and workers. Institute for Health Metrics and Evaluations: Around 66,000 people in the UK would die by August.",
+            "More than 38,000 cases; More than 2000 deaths; Around 13,000 have recovered; Most cases in Quebec with more than 20,000, Most recovered is Ontario with around 5,000; More than 1 million jobs have been lost. If those who do not comply with the 14 day self-isolation for returning travellers, there could be a fine up to $750,000 and 6 months of jail time.",
+            "Iran's Supreme Leader bans cultural, religious, and sports gatherings. More than 84,000 cases, 5000 deaths (6 people die/hour). Sanctions by the U.S has made it harder for Iran to obtain medical supplies",
+            "More than 204,000 cases; more than 82,500 recovered; more than 21,200 deaths; More than 900,000 masks and 1 million gloves will arrive in Spain from China; Wants EU to unite behind 1.5 trillion Euros in COVID-19 recovery fund; More than 3.5 million people are now unemployed",
+            "More than 183,000 cases; more than 51,600 recovered; more than 24,200 deaths; a quarter of deaths globally. Lockdown extended until May 3rd, 2020.  Healthcare workers make up 9% of COVID-19 cases",
+            "More than 82,700 cases; more than 4,600 deaths. China is currently dealing with a cluster of new cases from Russia; Heilongjiang, a province near Russia, has been locked down. China has provided medical aid to other countries; COVID-19 could kill $3.8 trillion new Silk Road",
+            "More than 819,800 cases; more than 75,000 recovered; more than 44,700 deaths. 8 states still have not ussed stay-at-home orders. New York has the highest amount of cases of more than 251,000. 6.6 million Americans are now jobless.",
+            "5G network is not the cause nor does it spread COVID-19, contrary to what some may suggest. The US military is not behind COVID-19, contrary to what some may suggest. Disney Plus did not release COVID-19 to gain more viewers, contrary to what some may suggest. Eating garlic does not prevent COVID-19, contrary to what some may suggest.",
+            "Stay at home, keep a safe distance, wash hands (at least 20 secs) often, cover your coughs and sneezes, stay at least 3m from someone who is feeling unwell. Do not touch your face",
+            "Drink a lot of fluids, keep warm, get lots of rest, use a hot humidifier or take a hot shower to ease sore throat and cough. No vaccine or cures.",
+            "It reached the isolated Yanomami tribe in the Amazon. There are more than 2.5 million cases; more than 177,000  deaths; more than 688,000 recovered. Active cases: 97% are mild casses (1.6 million) and 3% are critical or severe (57,000). Closed cases: 80% recovered or discharged (688,000) and 20% deaths (177,000). US has the most cases, Spain has the second most, and Italy has the third most." +
+                    "2" +
+                    "" +
+                    "" +
+                    "" +
+                    "" +
+                    "" +
+                    ""
+    };
+
+    public static int[] cost = {
+            0,
+            100,
+            0,
+            0,
+            100,
+            400,
+            200,
+            100,
+            200,
+            200,
+            400,
+            300,
+            400,
+            0,
+            300,
+            400
+    };
+
+}
